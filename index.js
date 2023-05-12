b30329f1a980d476097507dabf9849155af0720c
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2023-05-11 20:24:24 -0500

    Avances

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Seleccionamos todos los elementos de radio del grupo
 const opciones = document.querySelectorAll('input[name="opcion_simulador"]');
 const contenedores = document.querySelectorAll('.contenedor');
-console.log('Prueba');
-console.log(contenedores);
+const menu = document.querySelector('#menu');
 
 // Añadimos un evento de cambio a cada elemento de radio
 opciones.forEach((opcion) => {
@@ -10,7 +9,8 @@ opciones.forEach((opcion) => {
         for(let i = 0; i < contenedores.length; i++) {
             contenedores[i].style.display = 'none';
         }
+        menu.checked = false;
         contenedores[event.target.value].style.display = 'flex';
-        console.log(contenedores[event.target.value]);
+
     });
 });
