a9a58402e3ee818daa07aa83bc211746e5b0ff7b
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2023-05-12 21:48:50 -0500

    Avances porque quién se la aguanta

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ btn_calcular_cdt.addEventListener("click", () => {
     let resultado = calcularCDT(montoSimular, plazoSimular, tasaSimular);
 
     let resultado_cdt = document.getElementById('resultado_cdt');
-    resultado_cdt.innerHTML = `<p>Si inviertes ${'$'+monto.value} a ${plazo.value} meses recibirás ganancias por ${'$'+Intl.NumberFormat('de-DE').format(resultado)}</p><p>Ten en cuenta que debes restar la retenfuente y el valor final puede variar</p>`;
+    resultado_cdt.innerHTML = `<p>Si inviertes ${'$'+monto.value} a ${plazo.value} meses recibirás ganancias por ${'$'+Intl.NumberFormat('de-DE').format(resultado * 0.96)}</p><br><p>Ten en cuenta a este valor ya se le restó la retenfuente (4% sobre tus ganancias)</p>`;
 })
 
 function calcularCDT(monto, plazo, tasa) {
@@ -128,18 +128,30 @@ function calcularCuota(monto, plazo, tasa) {
 let calcular_ahorro = document.getElementById('calcular_ahorro'); 
 calcular_ahorro.addEventListener("click", () => {
 
-    let nombre = document.getElementById('nombreAhorro');
+    let montoAhorro = document.getElementById('montoAhorro');
+    let cuotasAhorro = document.getElementById('cuotasAhorro');
     let valorCuota = document.getElementById('valorAhorro');
     let frecuenciaAhorro = document.getElementById('frecuenciaAhorro');
+
+    let montoSimular = montoAhorro.value.replaceAll(".", "");
+    montoSimular = montoSimular.replaceAll(",", "");
+    montoSimular = parseInt(montoSimular);
+
+    let cuotasSimular = cuotasAhorro.value.replaceAll(".", "");
+    cuotasSimular = cuotasSimular.replaceAll(",", "");
+    cuotasSimular = parseInt(cuotasSimular);
 
     let valorSimular = valorCuota.value.replaceAll(".", "");
     valorSimular = valorSimular.replaceAll(",", "");
     valorSimular = parseInt(valorSimular);
 
-    // if (isNaN(montoSimular) || isNaN(plazoSimular) || isNaN(tasaSimular)) { alert("Por favor valida todos los datos ingresados"); return;}
+    if (isNaN(montoSimular) || isNaN(cuotasSimular) || isNaN(valorSimular) || frecuenciaAhorro.value == '') { alert("Por favor valida todos los datos ingresados"); return;}
 
     let periocidad = "";
     let factor = 1;
+
+    let tasa = calcularTIEA(montoSimular, cuotasSimular, valorSimular);
+
 
     switch (frecuenciaAhorro.value){
         case "Diario":
@@ -162,13 +174,32 @@ calcular_ahorro.addEventListener("click", () => {
     
     let resultado = valorSimular / factor;
     let resultado_ahorro = document.getElementById('resultado_ahorro');
-    resultado_ahorro.innerHTML = `<h2>${nombre.value}</h2><p>Si tienes responsabilidades por  ${'$'+valorCuota.value} tendrás que ahorrar aproximadamente ${periocidad} ${'$'+Intl.NumberFormat('de-DE').format(resultado)}</p><p>Recuerda que cumplir tus responsabilidades financieras te abre puertas</p>`;
+    resultado_ahorro.innerHTML = `<p>${tasa}Si tienes responsabilidades por  ${'$'+valorCuota.value} tendrás que ahorrar aproximadamente ${periocidad} ${'$'+Intl.NumberFormat('de-DE').format(resultado)}</p><p>Recuerda que cumplir tus responsabilidades financieras te abre puertas</p>`;
 })
+
+function calcularTasa(monto, cuotas, cuota) {
+    let tasa = 0.01
+    while (true) {
+      const numerador = cuota * ((1 + tasa) ** cuotas - 1)
+      const denominador = monto * ((1 + tasa) ** cuotas) - 1
+        const nuevaTasa = tasa - (numerador / denominador)
+        if (Math.abs(nuevaTasa - tasa) < 0.0001) {
+            break
+        }
+        tasa = nuevaTasa
+    }
+    return tasa * 100
+}
+
+function calcularTIEA(monto, cuotas, valorCuota) {
+    const i = (valorCuota / monto - 1) * 100;
+    const n = cuotas;
+    const tiea = ((1 + i / 100) ** n - 1) * 100;
+    return tiea.toFixed(2); // Redondeamos a dos decimales
+}
 
 function calcularAhorro(monto, plazo, tasa) {
   const interesMensual = tasa / 1200; // 12 meses en un año, y dividimos la tasa anual por 12
   const cuotaMensual = monto * interesMensual / (1 - Math.pow(1 + interesMensual, -plazo));
   return cuotaMensual.toFixed(2); // Redondeamos el resultado a dos decimales
 }
-
-
